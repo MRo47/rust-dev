62701db3fd2208879c6c40fbdb6927b2cb2093e8
Myron Rodrigues <rodriguesmyron47@gmail.com>
2025-02-24 11:47:07 +0000

    comments for reference later

--- rust-ws/guessing_game/src/main.rs
+++ rust-ws/guessing_game/src/main.rs
@@ -10,17 +10,24 @@ fn main() {
     loop {
         println!("guess a number");
 
+        // mutable type, default types are immutable
+        // type is guessed automatically here
         let mut guess = String::new();
 
+        // handle error result
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
+        // shadowing to reinit the variable
+        // match expression to handle result and error
+        // type specification unsigned int 32
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
+        // cmp 2 numbers will produce results of 3 types
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
